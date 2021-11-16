be836b5a17ce7b5b72a0f97c91d0cae0745d7885
saher <saher@satania>
2021-11-16 21:48:03 +0300

    outline for TeamCompetition. needs more work

---
+++ src/TeamCompetition.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class TeamCompetition extends Competition {
+
+    // The results of the competition.
+    // We use <Team, String> instead of <Team, Integer> because the
+    // competition might use Strings like "gold" or "silver".
+    HashMap<Team, String> results;
+
+    TeamCompetition(){
+        this.results = new HashMap<Team,String>();
+    }
+}
