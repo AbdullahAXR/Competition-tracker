8140c431ce87fce429db6f1802249fa3c2eff2d3
saher <saher@satania>
2021-11-16 21:43:31 +0300

    added Student.java

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String name;
+    private String id;
+    private String major;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getMajor(){
+        return major;
+    }
+
+
+}
