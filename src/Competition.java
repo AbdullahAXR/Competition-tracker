68a6222395c9f1381fe1480d1436826bb42e6d2a
saher <saher@satania>
2021-11-16 21:45:47 +0300

    outline for the Competition class. still needs work

---
+++ src/Competition.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public abstract class Competition {
+    String name;
+    String link;
+    Date date;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getLink(){
+        return link;
+    }
+
+    // return a *clone* of the date object instead of the actual date.
+    // If you want to change the actual date, then use setDate(Date newDate)
+    public Date getDate(){
+        return (Date) date.clone();
+    }
+
+    public void setDate(Date newdate){
+        this.date = newdate;
+    }
+
+}
