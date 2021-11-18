8c7f4082fba5cd41face2fa5cf7d28bcd20ff98a
saher <saher@satania>
2021-11-18 20:36:46 +0300

    added a new team constructor

--- src/Team.java
+++ src/Team.java
@@ -2,12 +2,18 @@ import java.util.*;
 public class Team extends Participant {
 
     // A team is just a number of students under one name
-    private ArrayList<Student> members;
+    private ArrayList<Student> students;
 
     Team(String name){
         super(name);
-        this.members = new ArrayList<Student>();
+        this.students = new ArrayList<Student>();
     }
+
+    Team(String name, ArrayList<Student> students){
+        super(name);
+        this.students = students;
+    }
+
 
     public String getName(){
         return name;
