be469cceab23fbf3d92baa50545612573938e253
saher <saher@satania>
2021-11-21 18:58:56 +0300

    fixed another typo

--- src/TeamCompetition.java
+++ src/TeamCompetition.java
@@ -8,6 +8,6 @@ public class TeamCompetition extends Competition {
 
     TeamCompetition(){
         this.results = new HashMap<Team,String>();
-        this.type = CompetitionType.TEAM;
+        this.type = Type.TEAM;
     }
 }
