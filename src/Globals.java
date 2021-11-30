635583d2827f6d6f72adb00afe213708ef0f8f66
saher <saher@satania>
2021-11-30 23:56:03 +0300

    added more globals

--- src/Globals.java
+++ src/Globals.java
@@ -1,4 +1,10 @@
+import java.util.Date;
+
 public class Globals {
     public final static CompetitionManager MANAGER = new CompetitionManager();
+    public final static int SPACING = 5; // used by [HV]Box
+
+    public final static Date NOW = new Date();
+
 }
 
