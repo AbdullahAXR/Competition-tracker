d26c22f48ec84d19bb2f58917d2c880b6cb29d52
saher <saher@satania>
2021-11-30 10:22:20 +0300

    The formatting in the builder is now pretty much the same as specified in the project files. The code is a display of concept, so its a little messy. I will try to make the class static. I'll create a seperate branch for that.

--- src/CompetitionSheetBuilder.java
+++ src/CompetitionSheetBuilder.java
@@ -14,14 +14,23 @@ public class CompetitionSheetBuilder<T extends Competition<? extends Participant
     private T c; 
     private XSSFRow prerow;
 
-    CompetitionSheetBuilder(XSSFSheet sheet, T c){
+    // cel style of participants
+    static XSSFCellStyle style;
+
+    CompetitionSheetBuilder(XSSFSheet sheet, T c) throws Exception {
 
         // used globally
         this.sheet = sheet;
         this.c = c;
         this.prerow = sheet.createRow(Specs.FIRST_PARTICIPANT_ROW-1); // this is used two times, so we just create a variable for it.
-    }
 
+        XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream("data.xlsx"));
+        XSSFCellStyle sampleStyle = wb.getSheetAt(0).getRow(Specs.FIRST_PARTICIPANT_ROW).getCell(Specs.ID_CELL).getCellStyle();
+        wb.close();
+
+        this.style = sheet.getWorkbook().createCellStyle();
+        this.style.cloneStyleFrom(sampleStyle);
+    }
 
     public static void main(String[] args) throws Exception {
 		XSSFWorkbook workbook = new XSSFWorkbook(new File("data.xlsx"));
@@ -30,14 +39,19 @@ public class CompetitionSheetBuilder<T extends Competition<? extends Participant
 
         TeamCompetition c0 = (TeamCompetition) CompetitionSheetParser.getCompetition(sheet0);
         StudentCompetition c1 = (StudentCompetition) CompetitionSheetParser.getCompetition(sheet1);
-        XSSFSheet newSheet = workbook.createSheet();
+        XSSFSheet newSheet0 = workbook.createSheet();
+        XSSFSheet newSheet1= workbook.createSheet();
         // System.out.println((StudentCompetition)CompetitionSheetParser.getCompetition(sheet0));
 
-        CompetitionSheetBuilder<TeamCompetition> cb = new CompetitionSheetBuilder<TeamCompetition>(newSheet, c0);
-        newSheet = cb.buildSheet();
+        CompetitionSheetBuilder<TeamCompetition> cb0 = new CompetitionSheetBuilder<TeamCompetition>(newSheet0, c0);
+        CompetitionSheetBuilder<StudentCompetition> cb1 = new CompetitionSheetBuilder<StudentCompetition>(newSheet1, c1);
+
+        newSheet0 = cb0.buildSheet();
+        newSheet1 = cb1.buildSheet();
         workbook.write(new FileOutputStream("temp.xlsx"));
         // StudentCompetition sc = (StudentCompetition) CompetitionSheetParser.getCompetition(newSheet);
-        // System.out.println((StudentCompetition)CompetitionSheetParser.getCompetition(newSheet));
+        // System.out.println((TeamCompetition)CompetitionSheetParser.getCompetition(newSheet0));
+        // System.out.println((StudentCompetition)CompetitionSheetParser.getCompetition(newSheet1));
 
     }
 
@@ -53,6 +67,7 @@ public class CompetitionSheetBuilder<T extends Competition<? extends Participant
             insertStudents();
         }
 
+        autoSizeColumns();
         return sheet;
     }
 
@@ -77,14 +92,22 @@ public class CompetitionSheetBuilder<T extends Competition<? extends Participant
 
     private void insertCommonPreRow() {
         // notice 0 is skipped
+
         prerow.createCell(1).setCellValue("Student ID");
         prerow.createCell(2).setCellValue("Student Name");
         prerow.createCell(3).setCellValue("Major");
+
+        prerow.createCell(0).setCellStyle(this.style); // we style this cell even though its empty
+        prerow.getCell(1).setCellStyle(this.style);
+        prerow.getCell(2).setCellStyle(this.style);
+        prerow.getCell(3).setCellStyle(this.style);
+
     }
 
     private void insertStudentCompetitionPreRow() {
         insertCommonPreRow();
         prerow.createCell(Specs.INDIVIDUAL_RANK_CELL).setCellValue("Rank");
+        prerow.getCell(Specs.INDIVIDUAL_RANK_CELL).setCellStyle(this.style);
     }
 
     private void insertTeamCompetitionPreRow(){
@@ -92,6 +115,10 @@ public class CompetitionSheetBuilder<T extends Competition<? extends Participant
         prerow.createCell(4).setCellValue("team");
         prerow.createCell(5).setCellValue("Team Name");
         prerow.createCell(Specs.TEAM_RANK_CELL).setCellValue("Rank");
+
+        prerow.getCell(4).setCellStyle(this.style);
+        prerow.getCell(5).setCellStyle(this.style);
+        prerow.getCell(Specs.TEAM_RANK_CELL).setCellStyle(this.style);
     }
 
     public void insertStudent(XSSFRow r, Student s, int index){
@@ -99,6 +126,11 @@ public class CompetitionSheetBuilder<T extends Competition<? extends Participant
         r.createCell(1, CellType.STRING).setCellValue(s.getId());
         r.createCell(2).setCellValue(s.getName());
         r.createCell(3).setCellValue(s.getMajor());
+
+        r.getCell(0).setCellStyle(this.style);
+        r.getCell(1).setCellStyle(this.style);
+        r.getCell(2).setCellStyle(this.style);
+        r.getCell(3).setCellStyle(this.style);
     }
 
     // with rank
@@ -117,6 +149,7 @@ public class CompetitionSheetBuilder<T extends Competition<? extends Participant
             insertStudent(r, s, index);
             rank = ((StudentCompetition) c).getResult(s);
             r.createCell(Specs.INDIVIDUAL_RANK_CELL, CellType.STRING).setCellValue(rank);
+            r.getCell(Specs.INDIVIDUAL_RANK_CELL).setCellStyle(this.style);
             i++;
 
         }
@@ -140,14 +173,30 @@ public class CompetitionSheetBuilder<T extends Competition<? extends Participant
             for(Student s : t){
                 r = sheet.createRow(i);
                 r.createCell(Specs.TEAM_INDEX_CELL).setCellValue(teamIndex);
+                r.getCell(Specs.TEAM_INDEX_CELL).setCellStyle(this.style);
+
                 r.createCell(Specs.TEAM_NAME_CELL).setCellValue(teamName);
+                r.getCell(Specs.TEAM_NAME_CELL).setCellStyle(this.style);
+
                 r.createCell(Specs.TEAM_RANK_CELL, CellType.STRING).setCellValue(teamRank);
+                r.getCell(Specs.TEAM_RANK_CELL).setCellStyle(this.style);
+
                 insertStudent(r, s, studentIndex++);
                 i++;
             }
             teamIndex++;
             // r = sheet.getRow(++i);
         }
+    }
+
+    public void autoSizeColumns(){
+        sheet.autoSizeColumn(0);
+        sheet.autoSizeColumn(1);
+        sheet.autoSizeColumn(2);
+        sheet.autoSizeColumn(3);
+        sheet.autoSizeColumn(4);
+        sheet.autoSizeColumn(5);
+        sheet.autoSizeColumn(6);
     }
 
 }
