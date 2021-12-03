15725d6f27eac8ee6a819736314a64002e0f5a13
AbdullahAXR <AbdullahXR511@gmail.com>
2021-12-03 21:56:53 +0300

    I created a class for the CompeitionList which has the basic structre in it

---
+++ src/CompetitionList.java
@@ -0,0 +1,37 @@
+import java.awt.Button;
+
+import javafx.scene.layout.Pane;
+
+public class CompetitionList {
+	public Button addButton;
+	public Button deleteButton;
+	public Pane competitionView;
+	public Competition currentCompetition;
+	
+	public void addButtonClicked() {
+		
+	}
+	
+	public void deleteButtonClicked() {
+		
+	}
+	
+	public void competitionCellClicked() {
+		
+	}
+	public void layoutUI() {
+		
+	}
+	
+	public void addCompetition() {
+		
+	}
+	
+	public void selectCompetition(Competition<Participant> competition) {
+		
+	}
+	
+	public void updateCompeitionList() {
+		
+	}
+}
