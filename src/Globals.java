6c8afeae60e1fe4ff99459139e8bf9772c009412
Mo7aMmAdx <im7mdxgs11@hotmail.com>
2021-12-05 23:23:55 +0300

    change selectedCompetition to be the first competition in the list

--- src/Globals.java
+++ src/Globals.java
@@ -5,7 +5,7 @@ import javafx.collections.FXCollections;
 public class Globals {
     public static CompetitionManager MANAGER = new CompetitionManager();
     public static ObservableList<Competition<?>> competitions = FXCollections.observableArrayList(MANAGER.readCompetitions());
-    public static Competition<?> currentCompetition = null; // CompetitionList will update this
+    public static Competition<?> currentCompetition = competitions.get(0); // CompetitionList will update this
     public final static int SPACING = 5; // used by [HV]Box
     public final static Date NOW = new Date();
 }
