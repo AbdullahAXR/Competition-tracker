993fa4661b5cbbb151f5001536ea7d49a8eda01c
saher <saher@satania>
2021-12-05 20:13:09 +0300

    added importnat comments in the statusbar

--- src/StatusBar.java
+++ src/StatusBar.java
@@ -11,21 +11,30 @@ public class StatusBar extends HBox {
 
     StatusBar(int spacing){
         super(spacing);
-        updateStatusBar(); // we might tun this whenever there's a change
+        // updateStatusBar(); // we might tun this whenever there's a change
     }
 
     StatusBar(){
         this(0);
     }
 
-    public void updateStatusBar() {
-        numberOfTeamCompetitions = Globals.MANAGER.getNumberOfTeamCompetition();
-        numberOfStudentCompetitions = Globals.MANAGER.size() - Globals.MANAGER.getNumberOfTeamCompetition();
-        numberOfDue = Globals.MANAGER.getNumberOfDue();
-        // this.getChildren().add(new Label(MANAGER.get(0).toString()));
-        this.getChildren().add(new Label("Student Competitions: "+numberOfStudentCompetitions));
-        this.getChildren().add(new Label("Team Competitions: "+numberOfTeamCompetitions));
-        this.getChildren().add(new Label("Due Competitions: "+numberOfDue));
-    }
+    // This method sucks, find a way to listen to changes
+    // Start from here https://docs.oracle.com/javase/8/javafx/api/javafx/collections/ListChangeListener.Change.html
+    // Basically, create a loop that groes through the changes and updates three
+    // varaibles numOfDue, numOfTeam, and numOfStudent. It shouldn't be too
+    // hard, follow the example in the link and **READ** the documentation
+
+    // observableArrayList
+    // public void updateStatusBar() {
+    //     numberOfTeamCompetitions = Globals.MANAGER.getNumberOfTeamCompetition();
+    //     numberOfStudentCompetitions = Globals.MANAGER.size() - Globals.MANAGER.getNumberOfTeamCompetition();
+    //     numberOfDue = Globals.MANAGER.getNumberOfDue();
+    //     // this.getChildren().add(new Label(MANAGER.get(0).toString()));
+    //     this.getChildren().add(new Label("Student Competitions: "+numberOfStudentCompetitions));
+    //     this.getChildren().add(new Label("Team Competitions: "+numberOfTeamCompetitions));
+    //     this.getChildren().add(new Label("Due Competitions: "+numberOfDue));
+    // }
+
+    // public stat
 
 }
