dddd62656f1d4daccf0920dc95134d5519f312f9
saher <saher@satania>
2021-12-05 19:08:55 +0300

    CompetitionList now extends ListView and has a listner that listens to changes in the listview and updates a global varaible currentCompetition

--- src/CompetitionList.java
+++ src/CompetitionList.java
@@ -1,37 +1,53 @@
 import java.awt.Button;
-
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
 
-public class CompetitionList {
-	public Button addButton;
-	public Button deleteButton;
-	public Pane competitionView;
-	public Competition<?> currentCompetition;
-	
-	public void addButtonClicked() {
-		
-	}
-	
-	public void deleteButtonClicked() {
-		
-	}
-	
-	public void competitionCellClicked() {
-		
-	}
-	public void layoutUI() {
-		
-	}
-	
-	public void addCompetition() {
-		
-	}
-	
-	public void selectCompetition(Competition<Participant> competition) {
-		
-	}
-	
-	public void updateCompeitionList() {
-		
-	}
+public class CompetitionList extends ListView<Competition<?>> {
+    // public Button addButton;
+    // public Button deleteButton;
+    // public Pane competitionView;
+    // public Competition<?> currentCompetition;
+
+    CompetitionList(){
+        this(null);
+
+    }
+
+
+    CompetitionList(ObservableList<Competition<?>> comps){
+        super(comps);
+        this.getSelectionModel().selectedItemProperty().addListener( (observable, oldv, newv) ->
+                {
+                    Globals.currentCompetition = newv;
+                    System.out.println(Globals.currentCompetition);
+                });
+    }
+
+    public void addButtonClicked() {
+
+    }
+
+    public void deleteButtonClicked() {
+
+    }
+
+    public void competitionCellClicked() {
+
+    }
+    public void layoutUI() {
+
+    }
+
+    public void addCompetition() {
+
+    }
+
+    public void selectCompetition(Competition<Participant> competition) {
+
+    }
+
+    public void updateCompeitionList() {
+
+    }
 }
--- src/Globals.java
+++ src/Globals.java
@@ -1,7 +1,11 @@
 import java.util.Date;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 
 public class Globals {
-    public final static CompetitionManager MANAGER = new CompetitionManager();
+    public static CompetitionManager MANAGER = new CompetitionManager();
+    public static ObservableList<Competition<?>> competitions = FXCollections.observableArrayList(MANAGER.readCompetitions());
+    public static Competition<?> currentCompetition = null; // CompetitionList will update this
     public final static int SPACING = 5; // used by [HV]Box
     public final static Date NOW = new Date();
 }
