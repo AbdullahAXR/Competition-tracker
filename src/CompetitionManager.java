41cb6704314f36c88baf5e25fcfe9ae4d375095c
saher <saher@satania>
2021-12-05 17:26:01 +0300

    refactored competitionManaget to be more static

--- src/CompetitionManager.java
+++ src/CompetitionManager.java
@@ -12,7 +12,7 @@ import java.util.Collections;
 
 public class CompetitionManager {
 
-    public ArrayList<Competition<?>> competitions = new ArrayList<>();
+    // public ArrayList<Competition<?>> competitions = new ArrayList<>();
     private final File dataFile = new File("temp.xlsx");
     private XSSFWorkbook dataWorkbook; 
 
@@ -23,8 +23,8 @@ public class CompetitionManager {
                 dataFile.createNewFile();
 
             this.dataWorkbook = new XSSFWorkbook(dataFile);
-            this.readCompetitions(); 
         }
+
         catch(IOException e) {
             System.exit(1);  // if file can not be created, or the workbook closed, then exit.
         }
@@ -36,19 +36,22 @@ public class CompetitionManager {
 
     public static void main(String[] args) throws Exception {
         CompetitionManager cm = new CompetitionManager();
-        System.out.println(cm.competitions.size());
+        ArrayList<Competition<?>> comps =  cm.readCompetitions();
+        System.out.println(comps);
+        System.out.println(comps.size());
 
-        LinkedHashMap<Student, String> lhm =  new LinkedHashMap<Student, String>();
-        lhm.put(new Student("20238531", "saher's friend", "MATH"), "1");
-        lhm.put(new Student("20238531", "saher's other friend", "swe"), "2");
-        lhm.put(new Student("20238531", "saher himself", "swe"), "-");
-        cm.add(new StudentCompetition("saher's cool competition", "www.saher.com", new Date(), lhm));
+        // LinkedHashMap<Student, String> lhm =  new LinkedHashMap<Student, String>();
+        // lhm.put(new Student("20238531", "saher's friend", "MATH"), "1");
+        // lhm.put(new Student("20238531", "saher's other friend", "swe"), "2");
+        // lhm.put(new Student("20238531", "saher himself", "swe"), "-");
+        // cm.add(new StudentCompetition("saher's cool competition", "www.saher.com", new Date(), lhm));
         // TeamCompetition tc = (TeamCompetition) cm.competitions.get(0);
-        cm.writeCompetitions();
+        // cm.writeCompetitions();
 
     }
 
-    private void readCompetitions() throws IOException {
+    public ArrayList<Competition<?>> readCompetitions() throws IOException {
+        ArrayList<Competition<?>> competitions = new ArrayList<>();
         XSSFSheet sheet;
 
         for(int i = 0; i < dataWorkbook.getNumberOfSheets(); i++){
@@ -58,11 +61,24 @@ public class CompetitionManager {
         }
 
         dataWorkbook.close();
+
+        return competitions;
     }
 
-    public void writeCompetitions() throws Exception {
+    public void writeCompetitions(ArrayList<Competition<?>> competitions) throws Exception {
 
-        XSSFCellStyle sampleStyle = dataWorkbook.getSheetAt(0).getRow(Specs.FIRST_PARTICIPANT_ROW).getCell(Specs.ID_CELL).getCellStyle();
+        // TODO: the followiong will cause an error if the the previous excel
+        // file is empty. You can work around it with a try catch block, but the
+        // style will be empty. To solve this issue, I storing the style object in a
+        // style.dat file, and then read it later.
+
+        XSSFCellStyle sampleStyle;
+        try {
+            sampleStyle = dataWorkbook.getSheetAt(0).getRow(Specs.FIRST_PARTICIPANT_ROW).getCell(Specs.ID_CELL).getCellStyle();
+        }
+        catch(IllegalArgumentException e){
+            sampleStyle = dataWorkbook.createCellStyle(); // empty style!
+        }
 
         dataWorkbook = new XSSFWorkbook();
         XSSFCellStyle style;
@@ -74,7 +90,7 @@ public class CompetitionManager {
 
         XSSFSheet sheet;
         Competition<?> c;
-        for(int i = 0; i < this.size(); i++){
+        for(int i = 0; i < competitions.size(); i++){
             c = competitions.get(i);
             String sheetName = c.getName();
 
@@ -95,49 +111,51 @@ public class CompetitionManager {
         dataWorkbook.write(new FileOutputStream(dataFile));
     }
 
-	// Replaces a question at index
-	public void set(int i, Competition<?> c) {
-		competitions.set(i, c);
-	}
+    //// why waste time remaking an arrayList? Bad idea
 
-	// Adds a question to the list
-	public void add(Competition<?> c) {
-		competitions.add(c);
-	}
+	// // Replaces a question at index
+	// public void set(int i, Competition<?> c) {
+	// 	competitions.set(i, c);
+	// }
 
-	public boolean isEmpty() {
-		return competitions.isEmpty();
-	}
+	// // Adds a question to the list
+	// public void add(Competition<?> c) {
+	// 	competitions.add(c);
+	// }
 
-	public int size() {
-		return competitions.size();
-	}
+	// public boolean isEmpty() {
+	// 	return competitions.isEmpty();
+	// }
 
-	public Competition<?> get(int i) {
-		return competitions.get(i);
-	}
+	// public int size() {
+	// 	return competitions.size();
+	// }
 
-	public void remove(int i) {
-		competitions.remove(i);
-	}
+	// public Competition<?> get(int i) {
+	// 	return competitions.get(i);
+	// }
 
-	public void remove(Competition<?> c) {
-		competitions.remove(c);
-	}
+	// public void remove(int i) {
+	// 	competitions.remove(i);
+	// }
+
+	// public void remove(Competition<?> c) {
+	// 	competitions.remove(c);
+	// }
 	
-	public int indexOf(Competition<?> c) {
-		return competitions.indexOf(c);
-	}
+	// public int indexOf(Competition<?> c) {
+	// 	return competitions.indexOf(c);
+	// }
 
-    public int getNumberOfTeamCompetition(){
-        return (int) competitions.stream() // .count() returns a long
-            .filter(c -> c instanceof TeamCompetition)
-            .count();
-    }
+    // public int getNumberOfTeamCompetition(){
+        // return (int) competitions.stream() // .count() returns a long
+            // .filter(c -> c instanceof TeamCompetition)
+            // .count();
+    // }
 
-    public int getNumberOfDue(){
-        return (int) competitions.stream()
-            .filter(c -> Globals.NOW.compareTo(c.getDate()) >= 0)
-            .count();
-    }
+    // public int getNumberOfDue(){
+        // return (int) competitions.stream()
+            // .filter(c -> Globals.NOW.compareTo(c.getDate()) >= 0)
+            // .count();
+    // }
 }
