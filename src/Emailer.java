832aa26947a48452d3539c9965882f97b1158349
saher <saher@satania>
2021-12-09 20:41:28 +0300

    Emailer works now!

--- src/Emailer.java
+++ src/Emailer.java
@@ -1,46 +1,53 @@
 import java.awt.Desktop;
 import java.net.URI;
+import java.net.URLEncoder;
+import java.net.URISyntaxException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets ;
 
 public class Emailer {
 
-    private URI uri;
+    // DON'T CHANGE THE SPELLING MISTAKES, THAT WOULD BE REQUIREMENT LEACKAGE.
+    private final static String SUBJECT = "Congratulation on achieving %s place in %s";
+    private final static String BODY = "Dear %s,\n\nConguratulation on your achievement in %s. This achievement is not deeply appreciated by the unversity and we will announce it in the approbrite medias.\n\nIn case you have Photos you want to share with the news post, reply to this email with the photos.\n\nRegards and Congrats,\nKFUPM News Team\n";
+
     private static Desktop desktop = Desktop.getDesktop();
 
-    // Emailer(URI uri){
-    //     this.uri = uri;
-    // }
+    // this is quite general, maybe it should be in its own class.
+    // Encode an email to a mailto String.
+    public static String encodeEmail(String address, String subject, String body){
+        String encoded = "mailto:"+address+"?subject="+URLEncoder.encode(subject, StandardCharsets.UTF_8)+
+            "&body="+URLEncoder.encode(body,StandardCharsets.UTF_8);
 
-    // private static void sendEmail(URI uri) throws IOException {
-    //     desktop.mail(uri);
-    // }
-
-
-    // public static void emailStudent(Student s) throws IOException {
-    //     String email = s.getEmail();
-    // }
-
-    public static void emailStudent(Student s, StudentCompetition c) throws IOException {
-        String sname= s.getName();
-        String cname = c.getName();
-
-
-        // TODO: we're supposed generate URIs from strings. Perhaps a better way to do
-        // this is by creating a static class URIMaker that has a method that
-        // takes a string and output a proper mailto link.
-        System.out.println(getSubject(c.getResult(s), cname));
-        System.out.println(getBody(sname, cname));
+        encoded = encoded.replace("+", "%20"); // URLEncoder replaces whitespace with +
+        return encoded;
     }
 
-    // See todo below
-    private static String getSubject(String rank, String competitionName){
-        return "Conguratulations on achieving "+rank+" place in "+competitionName;
-    }
+    public static void email(Competition<? extends Participant> c, Participant p) {
+        String competitionName = c.getName();
 
-    // TODO: this is just a proof of concept. Redo this using String.format()
-    // and store the body in a constant in globals. If you don't know
-    // String.format(), then look it up
-    private static String getBody(String participantName, String competitionName){
-        return "Dear "+participantName+",\n\nConguratulation on your achievement in "+competitionName+". This achievement is not deeply appreciated by the unversity and we will announce it in the approbrite medias.\n\nIn case you have Photos you want to share with the news post, reply to this email with the photos.\n\nRegards and Congrats,\nKFUPM News Team";
+        String rank;
+        // this is kinda dumb... Can we do better?
+        if(p instanceof Team)
+            rank = ((TeamCompetition)c).getResult((Team)p);
+        else 
+            rank = ((StudentCompetition)c).getResult((Student)p);
+
+        String addresses = String.join(",", p.getEmails());
+        String subject = String.format(SUBJECT, rank, competitionName);
+        String body = String.format(BODY, p.getName(), competitionName);
+        String encoded = encodeEmail(addresses, subject, body);
+
+        try {
+            URI uri = new URI(encoded);
+            desktop.mail(uri);
+        }
+        catch(URISyntaxException e){ // TODO: remove the catches later
+            System.out.println(e);
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+
     }
 }
